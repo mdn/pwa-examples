130d04f091a18d4e7081fbdbe7cbd5690dc28556
chrisdavidmills <chrisdavidmills@gmail.com>
2018-01-18 17:24:28 +0000

    changing theme of demo from frogs to foxes

--- a2hs/sw.js
+++ a2hs/sw.js
@@ -1,17 +1,21 @@
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('video-store').then(function(cache) {
-     return cache.addAll([
-       '/pwa-examples/a2hs/',
-       '/pwa-examples/a2hs/index.html',
-       '/pwa-examples/a2hs/index.js',
-       '/pwa-examples/a2hs/style.css',
-       '/pwa-examples/a2hs/images/pic1.jpg',
-       '/pwa-examples/a2hs/images/pic2.jpg',
-       '/pwa-examples/a2hs/images/pic3.jpg',
-       '/pwa-examples/a2hs/images/pic4.jpg'
-     ]);
-   })
+   // caches.open('video-store').then(function(cache) {
+   //   return cache.addAll([
+   //     '/pwa-examples/a2hs/',
+   //     '/pwa-examples/a2hs/index.html',
+   //     '/pwa-examples/a2hs/index.js',
+   //     '/pwa-examples/a2hs/style.css',
+   //     '/pwa-examples/a2hs/images/pic1.jpg',
+   //     '/pwa-examples/a2hs/images/pic2.jpg',
+   //     '/pwa-examples/a2hs/images/pic3.jpg',
+   //     '/pwa-examples/a2hs/images/pic4.jpg'
+   //   ]);
+   // })
+
+   caches.delete('video-store').then(function(boolean) {
+    alert('video-store cache deleted');
+  });
  );
 });
 
