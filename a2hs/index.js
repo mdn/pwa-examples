24e2cc5d74fa13c750183710e7f568bdd28be62c
Chris Mills <cmills@mozilla.com>
2018-10-24 12:40:07 +0100

    make a few further improvements ot the code

--- a2hs/index.js
+++ a2hs/index.js
@@ -39,8 +39,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-      .then((choiceResult) => {
+    deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
           console.log('User accepted the A2HS prompt');
         } else {
