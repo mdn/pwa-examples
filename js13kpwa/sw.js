a82549db6ccb7abd853fc6eac33e4ac21a98e88e
Chris Mills <cmills@mozilla.com>
2019-12-15 17:50:08 +0000

    update

--- js13kpwa/sw.js
+++ js13kpwa/sw.js
@@ -46,7 +46,7 @@ self.addEventListener('fetch', function(e) {
       console.log('[Service Worker] Fetching resource: '+e.request.url);
       return r || fetch(e.request).then(function(response) {
         return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          console.log('[Service Worker] Caching new resource: ' + e.request.url);
           cache.put(e.request, response.clone());
           return response;
         });
