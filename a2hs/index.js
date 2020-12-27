e60ff93c4ac498c791dc4d03609c5dfa412a2f53
Nino Filiu <nino.filiu@gmail.com>
2020-12-27 21:35:12 +0100

    lint a2hs/index.js

--- a2hs/index.js
+++ a2hs/index.js
@@ -1,22 +1,22 @@
-const images = ['fox1','fox2','fox3','fox4'];
+const images = ['fox1', 'fox2', 'fox3', 'fox4'];
 const imgElem = document.querySelector('img');
 
 function randomValueFromArray(array) {
-  let randomNo =  Math.floor(Math.random() * array.length);
+  const randomNo = Math.floor(Math.random() * array.length);
   return array[randomNo];
 }
 
-setInterval(function() {
-  let randomChoice = randomValueFromArray(images);
-  imgElem.src = 'images/' + randomChoice + '.jpg';
-}, 2000)
+setInterval(() => {
+  const randomChoice = randomValueFromArray(images);
+  imgElem.src = `images/${randomChoice}.jpg`;
+}, 2000);
 
 // Register service worker to control making site work offline
 
-if('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('/pwa-examples/a2hs/sw.js')
-           .then(function() { console.log('Service Worker Registered'); });
+    .register('/pwa-examples/a2hs/sw.js')
+    .then(() => { console.log('Service Worker Registered'); });
 }
 
 // Code to handle install prompt on desktop
@@ -33,19 +33,19 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Update UI to notify the user they can add to home screen
   addBtn.style.display = 'block';
 
-  addBtn.addEventListener('click', (e) => {
+  addBtn.addEventListener('click', () => {
     // hide our user interface that shows our A2HS button
     addBtn.style.display = 'none';
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
   });
 });
