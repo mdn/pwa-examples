0ab722ad62bb018667179f2ffb626de212a7ae25
Nino Filiu <nino.filiu@gmail.com>
2020-12-27 21:40:47 +0100

    lint js13kpwa/sw.js

--- js13kpwa/sw.js
+++ js13kpwa/sw.js
@@ -1,8 +1,8 @@
 self.importScripts('data/games.js');
 
 // Files to cache
-var cacheName = 'js13kPWA-v1';
-var appShellFiles = [
+const cacheName = 'js13kPWA-v1';
+const appShellFiles = [
   '/pwa-examples/js13kpwa/',
   '/pwa-examples/js13kpwa/index.html',
   '/pwa-examples/js13kpwa/app.js',
@@ -20,37 +20,35 @@ var appShellFiles = [
   '/pwa-examples/js13kpwa/icons/icon-168.png',
   '/pwa-examples/js13kpwa/icons/icon-192.png',
   '/pwa-examples/js13kpwa/icons/icon-256.png',
-  '/pwa-examples/js13kpwa/icons/icon-512.png'
+  '/pwa-examples/js13kpwa/icons/icon-512.png',
 ];
-var gamesImages = [];
-for(var i=0; i<games.length; i++) {
-  gamesImages.push('data/img/'+games[i].slug+'.jpg');
+const gamesImages = [];
+for (let i = 0; i < games.length; i++) {
+  gamesImages.push(`data/img/${games[i].slug}.jpg`);
 }
-var contentToCache = appShellFiles.concat(gamesImages);
+const contentToCache = appShellFiles.concat(gamesImages);
 
 // Installing Service Worker
-self.addEventListener('install', function(e) {
+self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then((cache) => {
       console.log('[Service Worker] Caching all: app shell and content');
       return cache.addAll(contentToCache);
-    })
+    }),
   );
 });
 
 // Fetching content using Service Worker
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(function(r) {
-      console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then(function(response) {
-        return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: ' + e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
-    })
+    caches.match(e.request).then((r) => {
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      return r || fetch(e.request).then((response) => caches.open(cacheName).then((cache) => {
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+      }));
+    }),
   );
 });
