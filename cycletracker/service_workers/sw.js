49ad41704657be56e957e599d02579f4eef5199e
Allo <yin199909@aliyun.com>
2023-09-20 13:31:08 +0800

    fix(cycletracker): run prettier format to align the style

--- cycletracker/service_workers/sw.js
+++ cycletracker/service_workers/sw.js
@@ -19,7 +19,7 @@ self.addEventListener("install", (event) => {
     (async () => {
       const cache = await caches.open(CACHE_NAME);
       cache.addAll(APP_STATIC_RESOURCES);
-    })()
+    })(),
   );
 });
 
@@ -33,10 +33,10 @@ self.addEventListener("activate", (event) => {
           if (name !== CACHE_NAME) {
             return caches.delete(name);
           }
-        })
+        }),
       );
       await clients.claim();
-    })()
+    })(),
   );
 });
 
@@ -61,6 +61,6 @@ self.addEventListener("fetch", (event) => {
         // If resource isn't in the cache, return a 404.
         return new Response(null, { status: 404 });
       }
-    })()
+    })(),
   );
 });
